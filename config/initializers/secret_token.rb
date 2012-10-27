8014c284ae7115d2035e5d386a34323786fdd77c
ecmartz <ecmartz.nm@gmail.com>
2012-10-27 15:13:56 -0600

    added everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeweler::Application.config.secret_token = '5a0b38e90734bc4aefa053925adfce51b6738a958078ff841b1dfda868d612c2dc485c89bfaf371892707016b404a49c6cf08bd603a9f5ffeb4c20ae6fd5c665'
